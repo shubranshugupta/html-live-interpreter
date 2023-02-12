01e8b03a69e06507af3ef1b413972c62ab1e5930
shubranshugupta <57756826+shubranshugupta@users.noreply.github.com>
2023-02-12 13:53:15 +0530

    remove manifest error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,10 +27,10 @@ function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link active" to="/about">About</Link>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
                     </ul>
                     <div className="mx-3 p-2">
